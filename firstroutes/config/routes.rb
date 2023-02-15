d00a0ef115f45e3b68211a5ce6c1b132a3c2660a
Steven Paalz <stevenpaalz@gmail.com>
2023-02-15 15:13:51 -0500

    set up routes

--- firstroutes/config/routes.rb
+++ firstroutes/config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :users
+
+  get "/users", to: "users#index"
+  get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  patch "/users/:id", to: "users#update"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  put "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy"
 end
