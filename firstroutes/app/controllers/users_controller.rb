f921287520ed396229926d0ae45a1fb7cda4a877
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-02-15 16:40:05 -0500

    controller

--- firstroutes/app/controllers/users_controller.rb
+++ firstroutes/app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
+
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all
     end
 
     def create
-        
-       render json: params 
+        user = User.new(params.require(:user).permit(:name,:email))
+        # replace the `user_attributes_here` with the actual attribute keys
+
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: 422
+        end
     end
 
     def show
         render json: params
     end
 end
+
